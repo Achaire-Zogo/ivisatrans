bf99c5171f3a255fafb0f88d4f0a0eee7795eb35
Achaire_Zogo <dylanabouma@gmail.com>
2022-08-18 14:33:22 +0100

    Update Translate.dart

--- lib/Screen/Dico/Translate.dart
+++ lib/Screen/Dico/Translate.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_tts/flutter_tts.dart';
 import 'package:translator/translator.dart';
 
@@ -33,6 +36,23 @@ class _TranslateState extends State<Translate> {
       print(out);
     });
   }
+
+  _search() async {
+    if (_controller.text == null || _controller.text.length == 0) {
+      // _streamController?.add(null);
+      return;
+    }
+    // var url = Uri.parse(_url + _controller.text.trim());
+    //
+    // _streamController?.add("waiting");
+    // Response response =
+    // await get(url, headers: {"Authorization": "Token " + _token});
+    // // print(response.body);
+    // _streamController?.add(json.decode(response.body));
+  }
+
+  TextEditingController _controller = TextEditingController();
+  Timer? _debounce;
 
   @override
   Widget build(BuildContext context) {
@@ -68,40 +88,67 @@ class _TranslateState extends State<Translate> {
             },
           ),
         ],
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(100.0),
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(left: 12.0, bottom: 8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(24.0),
+                  ),
+                  child: SingleChildScrollView(
+                    child: TextFormField(
+                      onChanged: (String text) {
+                        if (_debounce?.isActive ?? false) _debounce?.cancel();
+                        _debounce =
+                            Timer(const Duration(milliseconds: 1000), () {
+                          trans();
+                        });
+                      },
+                      keyboardType: TextInputType.multiline,
+                      maxLines: 2, //grow automatically
+                      controller: lang,
+                      // controller: _controller,
+                      decoration: const InputDecoration(
+                        hintText: "Search for a word",
+                        contentPadding: EdgeInsets.only(left: 24.0),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  trans();
+                  // _search();
+                },
+              )
+            ],
+          ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(10),
-              height: 200,
-              color: Colors.grey,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Center(
-                  child: TextField(
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null, //grow automatically
-                    controller: lang,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            RaisedButton(
-              color: Colors.red,
-              child: const Text(
-                  "Translate !!"), //on press to translate the language using function
-              onPressed: () {
-                trans();
-              },
+            SizedBox(
+              height: 15,
             ),
             Container(
-              padding: const EdgeInsets.all(7.0),
+              padding: const EdgeInsets.all(10.0),
               child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical, child: Text(out.toString())),
+                  scrollDirection: Axis.vertical,
+                  child: Text(
+                    out.toString(),
+                    style: TextStyle(fontSize: 18),
+                  )),
             ),
           ],
         ),
